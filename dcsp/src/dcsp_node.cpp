b22e6e1af171301777ff1d380db8533b1139c729
Dulitha93 <dulidab93@gmail.com>
2017-11-23 10:24:33 +0530

    Commented code for timeout

--- dcsp/src/dcsp_node.cpp
+++ dcsp/src/dcsp_node.cpp
@@ -12,6 +12,13 @@ int main(int argc, char **argv)
     dcsp_exploration::Agent dcspAgent(nh, nh_private);
 
     ROS_INFO("DCSP Agent Started.");
+    /*
+    double duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC; ////////
+    ROS_INFO("******************Duration: %f *****************************", duration);
+    if(duration >= atof(srv_time_out)){
+        ROS_INFO("Publish my topics");
+    }
+    */
     ros::spin();
     return 0;
 }
